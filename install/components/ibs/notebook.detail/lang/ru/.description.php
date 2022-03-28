c06e134c14bacea4d7ee26f6ec1bda14b2eab080
Vyacheslav <websstart@gmail.com>
2022-03-28 10:23:26 +0300

    FIX

--- install/components/ibs/notebook.detail/lang/ru/.description.php
+++ install/components/ibs/notebook.detail/lang/ru/.description.php
@@ -1,4 +1,4 @@
 <?
-$MESS['IBS_NOTEBOOK_COMPONENT_DETAIL'] = 'ноутбук детально';
+$MESS['IBS_NOTEBOOK_COMPONENT_DETAIL'] = 'Ноутбуки';
 $MESS['IBS_NOTEBOOK_COMPONENT_DETAIL_NAME'] = 'Детальная страница ноутбука';
 $MESS['IBS_NOTEBOOK_COMPONENT_DETAIL_DESCRIPTION'] = 'Компонент вывода детальной карточки ноутбука';
